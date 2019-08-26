9c6cbc979eafb106f52c5797adcccf8a3fda0d22
Jan Czerwinski <jan.czerwinski@ecom.software>
2019-08-26 15:53:45 +0200

    #4 Add statement component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from "react";
+import Statement from "./Components/Statement/Statement";
+
+
 
 const App: FC = () => {
-    return <div className="App">{/* All code goes here */}</div>;
+    return <div className="App">{/* All code goes here */}
+                <Statement text="przykladowy komunikat"/> 
+           </div>;
 };
 
 export default App;
---
+++ src/Components/Statement/Statement.tsx
@@ -0,0 +1,16 @@
+import React, {FC} from "react";
+
+interface StatementProps{
+    text: string;
+}
+
+
+const Statement: FC<StatementProps> = (props: StatementProps)=>{
+    return (
+        <div>
+            <p> {props.text} </p>
+        </div>
+    );
+}
+
+export default Statement;
