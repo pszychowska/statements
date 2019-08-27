85211fcb00a441173884d21db4e5a4c186c3a9a3
Jan Czerwiński <jan.ignacy.czerwinski@gmail.com>
2019-08-27 19:14:32 +0200

    #5 fix naming issues

--- src/components/StatementList/StatementList.tsx
+++ src/components/StatementList/StatementList.tsx
@@ -2,13 +2,13 @@ import React, { FC } from "react";
 import Statement from "../Statement/Statement";
 import { StatementModel } from "../../shared/StatementModel";
 
-interface StatementListProps {
+interface Statements {
     array: StatementModel[];
 }
 
-const StatementList: FC<StatementListProps> = (props: StatementListProps) => {
+const StatementList: FC<Statements> = (props: Statements) => {
     if (props.array.length > 0) {
-        const activeStatements = props.array.map(statement => <Statement key={statement.id} text={statement.text} />);
+        const activeStatements = props.array.map(statement => <Statement key={statement.id} text={statement.content} />);
         return <div>{activeStatements}</div>;
     }
     return <div>Brak komunikatów</div>;
--- src/shared/StatementModel.ts
+++ src/shared/StatementModel.ts
@@ -1,4 +1,4 @@
 export interface StatementModel {
     id: string;
-    text: string;
+    content: string;
 }
