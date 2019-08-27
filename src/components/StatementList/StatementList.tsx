2da95e6045d51bd39df491b565a39dac8d4ef411
Jan Czerwinski <jan.czerwinski@ecom.software>
2019-08-27 16:59:08 +0200

    #5 add statement array interface to shared file

--- src/components/StatementList/StatementList.tsx
+++ src/components/StatementList/StatementList.tsx
@@ -1,12 +1,8 @@
 import React, { FC } from "react";
 import Statement from "../Statement/Statement";
-import { StatementModel } from "../../shared/StatementModel";
+import { StatementModelArray } from "../../shared/StatementModelArray";
 
-interface StatementListProps {
-    array: StatementModel[];
-}
-
-const StatementList: FC<StatementListProps> = (props: StatementListProps) => {
+const StatementList: FC<StatementModelArray> = (props: StatementModelArray) => {
     if (props.array.length > 0) {
         const activeStatements = props.array.map(statement => <Statement key={statement.id} text={statement.text} />);
         return <div>{activeStatements}</div>;
---
+++ src/shared/StatementModelArray.ts
@@ -0,0 +1,5 @@
+import { StatementModel } from "./StatementModel";
+
+export interface StatementModelArray {
+    array: StatementModel[];
+}
