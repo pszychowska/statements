2442fb6b00f493a7b5a9ffb1a463f9829f369b8d
Paulina Szychowska <szychola85@gmail.com>
2019-08-29 09:48:31 +0200

    #17 change input text to textarea, fix classnames

--- src/components/adminPanel/AddStatementForm.tsx
+++ src/components/adminPanel/AddStatementForm.tsx
@@ -14,7 +14,7 @@ export default class AddStatementForm extends Component<{}, AddStatementFormStat
         endTime: ""
     };
 
-    private changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    private changeHandlerInput = (e: React.ChangeEvent<HTMLInputElement>) => {
         const key: string = e.currentTarget.name;
 
         if (Object.keys(this.state).includes(key)) {
@@ -22,36 +22,43 @@ export default class AddStatementForm extends Component<{}, AddStatementFormStat
         }
     };
 
+    private changeHandlerTextarea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const key: string = e.currentTarget.name;
+
+        if (Object.keys(this.state).includes(key)) {
+            this.setState({ [key]: e.currentTarget.value } as Pick<AddStatementFormState, keyof AddStatementFormState>);
+        }
+    };
     private submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
     };
 
     render() {
         return (
-            <form className={styles.adminPanelForm} onSubmit={this.submitHandler}>
-                <div className={styles.adminPanelText}>
-                    <p>{pl.adminPanel.labels.content}</p>
-                    <input name="content" type="text" onChange={this.changeHandler} value={this.state.content} />
+            <form className={styles.form} onSubmit={this.submitHandler}>
+                <div className={styles.text}>
+                    {pl.adminPanel.labels.content}
+                    <textarea name="content" onChange={this.changeHandlerTextarea} value={this.state.content} />
                 </div>
-                <div className={styles.adminPanelDate}>
-                    <p>{pl.adminPanel.labels.startDate}</p>
+                <div className={styles.date}>
+                    {pl.adminPanel.labels.startDate}
                     <input
                         name="startTime"
                         type="datetime-local"
-                        onChange={this.changeHandler}
+                        onChange={this.changeHandlerInput}
                         value={this.state.startTime}
                     />
                 </div>
-                <div className={styles.adminPanelDate}>
-                    <p>{pl.adminPanel.labels.endDate}</p>
+                <div className={styles.date}>
+                    {pl.adminPanel.labels.endDate}
                     <input
                         name="endTime"
                         type="datetime-local"
-                        onChange={this.changeHandler}
+                        onChange={this.changeHandlerInput}
                         value={this.state.endTime}
                     />
                 </div>
-                <button className={styles.adminPanelButton} type="submit">
+                <button className={styles.submit} type="submit">
                     {pl.adminPanel.labels.submit}
                 </button>
             </form>
