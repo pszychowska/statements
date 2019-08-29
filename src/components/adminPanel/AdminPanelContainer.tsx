d4085c59b208d07b4eb5f286c7a8554c8fe6e420
Paulina Szychowska <szychola85@gmail.com>
2019-08-29 10:17:30 +0200

    #17 add button style

--- src/components/adminPanel/AdminPanelContainer.tsx
+++ src/components/adminPanel/AdminPanelContainer.tsx
@@ -5,7 +5,7 @@ import styles from "./adminPanel.module.scss";
 export const AdminPanelContainer: FC<any> = (props: any) => {
     return (
         <div>
-            <h1 className={styles.adminPanelHeader}>Panel administracyjny</h1>
+            <h1 className={styles.header}>Panel administracyjny</h1>
             <AdminPanel />
         </div>
     );
